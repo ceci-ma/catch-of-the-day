a5b731d7c96c294205de338dd3f2f7595fe2644e
ceci-ma <ceciliamarcellesi@gmail.com>
2020-05-11 15:41:06 +0100

    Initial setup, html and css

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker.js";
+import "./css/style.css";
+
+alert("hey");
+render(<StorePicker />, document.querySelector("#main"));
