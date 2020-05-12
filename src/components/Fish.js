5929bd5a642ed678c97836e13e4a348f3a956e19
ceci-ma <ceciliamarcellesi@gmail.com>
2020-05-12 16:36:52 +0100

    update and display order state

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,15 +4,21 @@ import { formatPrice } from "../helpers";
 class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
-        <img src={this.props.details.image} alt={this.props.details.name} />
+        <img src={image} alt={name} />
         <h3 className="fish-name">
           {name}
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to cart</button>
+        <button
+          onClick={() => this.props.addToOrder(this.props.index)}
+          disabled={!isAvailable}
+        >
+          {isAvailable ? "Add to order" : "Sold Out"}
+        </button>
       </li>
     );
   }
