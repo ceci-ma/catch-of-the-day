1a5737dd6e37be03a63dc8f6abe9c0a53eec7ef8
ceci-ma <ceciliamarcellesi@gmail.com>
2020-05-12 17:24:36 +0100

    persisting order state with localStorage

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,6 +7,8 @@ class Order extends React.Component {
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === "available";
 
+    if (!fish) return null;
+
     if (!isAvailable) {
       return (
         <li key={key}>
