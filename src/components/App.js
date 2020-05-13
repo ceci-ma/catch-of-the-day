5cf1b69c5ce3947fdaee14f06d3843a31ac52a51
ceci-ma <ceciliamarcellesi@gmail.com>
2020-05-13 13:42:12 +0100

    add removing items from state

--- src/components/App.js
+++ src/components/App.js
@@ -56,6 +56,15 @@ class App extends React.Component {
     this.setState({ fishes: fishes });
   };
 
+  deleteFish = (key) => {
+    // 1. Take a copy og state
+    const fishes = { ...this.state.fishes };
+    // 2. Update the state
+    fishes[key] = null;
+    // 2. Set the new fishes object to state
+    this.setState({ fishes: fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -66,6 +75,15 @@ class App extends React.Component {
     // 2. Either add to order or update the number in our order
     order[key] = order[key] + 1 || 1;
     // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
+  removeFromOrder = (key) => {
+    // 1. Tale a copy of state
+    const order = { ...this.state.order };
+    // 2. Remove item from order
+    delete order[key];
+    // 3. Set the new order object to state
     this.setState({ order });
   };
 
@@ -85,12 +103,17 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
+          deleteFish={this.deleteFish}
         />
       </div>
     );
